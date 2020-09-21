1b162a00feaec64dffbd38d698753fa1171a7d1b
AmirArreaza <ajaarreazav@gmail.com>
2020-09-21 14:39:03 +0100

    Added the Modal and Backdrop show property

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 import Modal from "./components/Modal/Modal";
 import List from "./components/List/List";
 import Backdrop from "./components/Backdrop/Backdrop";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React Animations 101</h1>
-      <Modal />
-      <Backdrop />
-      <button className="Button">Open Modal</button>
-      <h3>Animating Lists</h3>
-      <List />
-    </div>
-  );
-}
+class App extends Component {
+  
+  state = {
+    modalIsOpen: false,
+  }
+  
 
+  showModal = () => {
+    this.setState({modalIsOpen: true});
+  }
+  closeModal = () => {
+    this.setState({modalIsOpen: false});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations 101</h1>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
+        <Backdrop show={this.state.modalIsOpen} />
+        <button className="Button" onClick={this.showModal}>Open Modal</button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+}
 export default App;
