c63aee2cabd6a7811abb212108563eacd0e626f6
AmirArreaza <ajaarreazav@gmail.com>
2020-09-21 14:22:48 +0100

    Added the base project boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Modal from "./components/Modal/Modal";
+import List from "./components/List/List";
+import Backdrop from "./components/Backdrop/Backdrop";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Animations 101</h1>
+      <Modal />
+      <Backdrop />
+      <button className="Button">Open Modal</button>
+      <h3>Animating Lists</h3>
+      <List />
     </div>
   );
 }
