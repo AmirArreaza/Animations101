281c3d6c51b88032b8ce8edb8e814539b5dcdec3
AmirArreaza <ajaarreazav@gmail.com>
2020-09-25 14:36:37 +0100

    Transitions events in the Transition component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,13 +5,23 @@ import "./Modal.css";
 
 const animationTiming = {
   enter: 400,
-  exit: 1000
-}
+  exit: 1000,
+};
 
 const modal = (props) => {
-
   return (
-    <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
+    <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      onEnter={() => console.log("On enter")}
+      onEntering={() => console.log("On entering")}
+      onEntered={() => console.log("On entered")}
+      onExit={() => console.log("On exit")}
+      onExiting={() => console.log("On exiting")}
+      onExited={() => console.log("On exited")}
+    >
       {(state) => {
         const cssClasses = [
           "Modal",
